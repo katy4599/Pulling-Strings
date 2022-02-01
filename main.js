4e9e7aa2a523f48c230d36c70986dcfcba17470b
katy millard <katy.millard@gmail.com>
2022-02-01 13:48:52 -0600

    Second commit

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 
 function main() {
-    //              0123456789AB
-    const myName = 'Katy Millard';
-    const mySplit = myName.split(' ');
-    const mid = 'Janelle';
-    const myFull = `Katy ${mid} Millard`;
-    const mySplitReverse = mySplit.reverse().join(' ');
+    const myMessage = 'Gunner Rocks!';
+    let result = ''
+    
+    for (let i = 0; i < myMessage.length; i += 1) {
+        const char = myMessage[i];
 
-    console.log(myName);
-    console.log(mySplit);
-    console.log(`Katy ${mid} Millard`);
-    console.log(myName.toUpperCase());
-    console.log(myName.toLowerCase());
-    console.log(mySplitReverse);
-    console.log(myName.slice(0, 2));
-    console.log(myName.slice(9));
-    console.log(myFull[0] + myFull[5] + myFull[13]);
+        result += `char ${ } char`;
+    }
+
+    console.log(result);
 }
 
 main()
